31b998fe64746f14f2f78e58ce269c9c777acc27
a.safronov <a.safronov@smartway.today>
2025-04-12 21:28:55 +0300

    Fix script

--- Services/RequestService/DataAccess/Repositories/Sql/Request/GetByBrigade.sql
+++ Services/RequestService/DataAccess/Repositories/Sql/Request/GetByBrigade.sql
@@ -1,11 +1,15 @@
 ﻿SELECT id AS "Id",
-       address AS "Address",
+       city AS "City",
+       street AS "Street",
+       room AS "Room",
+       flat AS "Flat"
        device AS "Device",
        status AS "Status",
        type AS "Type",
        creation_time AS "CreationTime",
        work_time AS "WorkTime",
        completion_time AS "CompletionTime",
-       brigade_id AS "BrigadeId"
+       brigade_id AS "BrigadeId",
+       geotag AS "GeoTag"
 FROM requests
 WHERE brigade_id = @BrigadeId
