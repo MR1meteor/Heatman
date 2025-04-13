4736524b7d751cfa4dfad8aa8126ff4410f87e37
a.safronov <a.safronov@smartway.today>
2025-04-13 12:53:57 +0300

    PLEASE

--- Services/ReportService/Services/ActFileProcessor.cs
+++ Services/ReportService/Services/ActFileProcessor.cs
@@ -13,61 +13,63 @@ public class ActFileProcessor : IActFileProcessor
         var tempFile = Path.GetTempFileName().Replace(".tmp", ".docx");
         File.Copy(templatePath, tempFile, overwrite: true);
 
-        using var doc = WordprocessingDocument.Open(tempFile, true);
-        var body = doc.MainDocumentPart!.Document.Body!;
+        using (var doc = WordprocessingDocument.Open(tempFile, true))
+        {
+            var body = doc.MainDocumentPart!.Document.Body!;
 
-        ReplacePlaceholder(body, "{{Address}}", act.Address);
-        
-        if (act.HasCommutingDevice)
-        {
-            ReplacePlaceholder(body, "{{HasCommutingDevice}}", "X");
-            ReplacePlaceholder(body, "{{HasNotCommutingDevice}}", "");
-        }
-        else
-        {
-            ReplacePlaceholder(body, "{{HasCommutingDevice}}", "");
-            ReplacePlaceholder(body, "{{HasNotCommutingDevice}}", "X");
-        }
-        
-        if (act.HasViolation)
-        {
-            ReplacePlaceholder(body, "{{HasViolations}}", "X");
-            ReplacePlaceholder(body, "{{HasNotViolations}}", "");
-        }
-        else
-        {
-            ReplacePlaceholder(body, "{{HasViolations}}", "");
-            ReplacePlaceholder(body, "{{HasNotViolations}}", "X");
-        }
-        
-        switch (act.MeteringDeviceLocationType)
-        {
-            case MeteringDeviceLocation.OnLanding:
-                ReplacePlaceholder(body, "{{LocationOnLanding}}", "X");
-                ReplacePlaceholder(body, "{{LocationInFlat}}", "");
-                break;
-            case MeteringDeviceLocation.InFlat:
-                ReplacePlaceholder(body, "{{LocationOnLanding}}", "");
-                ReplacePlaceholder(body, "{{LocationInFlat}}", "X");
-                break;
-            default:
-                ReplacePlaceholder(body, "{{LocationOnLanding}}", "");
-                ReplacePlaceholder(body, "{{LocationInFlat}}", "");
-                break;
-        }
-        
-        ReplacePlaceholder(body, "{{DeviceReadings}}", act.DeviceReadings);
-        ReplacePlaceholder(body, "{{FirstInspector}}", act.Workers[0]);
-        ReplacePlaceholder(body, "{{SecondInspector}}", act.Workers[1]);
+            ReplacePlaceholder(body, "{{Address}}", act.Address);
 
-        if (act.HasViolation)
-        {
-            // ReplaceDrawingWithImage(doc, "checkbox_hasNotViolation", "Templates/checked.png");
-            // ReplacePlaceholder(body, "{{Check}}", "X");
+            if (act.HasCommutingDevice)
+            {
+                ReplacePlaceholder(body, "{{HasCommutingDevice}}", "X");
+                ReplacePlaceholder(body, "{{HasNotCommutingDevice}}", "");
+            }
+            else
+            {
+                ReplacePlaceholder(body, "{{HasCommutingDevice}}", "");
+                ReplacePlaceholder(body, "{{HasNotCommutingDevice}}", "X");
+            }
+
+            if (act.HasViolation)
+            {
+                ReplacePlaceholder(body, "{{HasViolations}}", "X");
+                ReplacePlaceholder(body, "{{HasNotViolations}}", "");
+            }
+            else
+            {
+                ReplacePlaceholder(body, "{{HasViolations}}", "");
+                ReplacePlaceholder(body, "{{HasNotViolations}}", "X");
+            }
+
+            switch (act.MeteringDeviceLocationType)
+            {
+                case MeteringDeviceLocation.OnLanding:
+                    ReplacePlaceholder(body, "{{LocationOnLanding}}", "X");
+                    ReplacePlaceholder(body, "{{LocationInFlat}}", "");
+                    break;
+                case MeteringDeviceLocation.InFlat:
+                    ReplacePlaceholder(body, "{{LocationOnLanding}}", "");
+                    ReplacePlaceholder(body, "{{LocationInFlat}}", "X");
+                    break;
+                default:
+                    ReplacePlaceholder(body, "{{LocationOnLanding}}", "");
+                    ReplacePlaceholder(body, "{{LocationInFlat}}", "");
+                    break;
+            }
+
+            ReplacePlaceholder(body, "{{DeviceReadings}}", act.DeviceReadings);
+            ReplacePlaceholder(body, "{{FirstInspector}}", act.Workers[0]);
+            ReplacePlaceholder(body, "{{SecondInspector}}", act.Workers[1]);
+
+            if (act.HasViolation)
+            {
+                // ReplaceDrawingWithImage(doc, "checkbox_hasNotViolation", "Templates/checked.png");
+                // ReplacePlaceholder(body, "{{Check}}", "X");
+            }
+
+            doc.MainDocumentPart.Document.Save();
         }
-        
-        doc.MainDocumentPart.Document.Save();
-        
+
         var fileBytes = File.ReadAllBytes(tempFile);
         File.Delete(tempFile);
         
