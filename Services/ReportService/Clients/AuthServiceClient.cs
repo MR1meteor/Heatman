7f34af18999c2c2ef94915809b20d16f49af929d
a.safronov <a.safronov@smartway.today>
2025-04-13 11:51:14 +0300

    Fix route

--- Services/ReportService/Clients/AuthServiceClient.cs
+++ Services/ReportService/Clients/AuthServiceClient.cs
@@ -20,7 +20,7 @@ public class AuthServiceClient : IAuthServiceClient
     
     public async Task<Result<List<User>>> GetUsersByIdsAsync(IEnumerable<Guid> ids)
     {
-        var response = await _httpClient.GetAsync($"{_baseUrl}/api/auth/user/by-ids?ids={string.Join(',', ids)}");
+        var response = await _httpClient.GetAsync($"{_baseUrl}/api/auth/user/by-ids?ids={string.Join("&ids=", ids)}");
         var responseContent = await response.Content.ReadAsStringAsync();
 
         if (!response.IsSuccessStatusCode)
